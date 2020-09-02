cac493e7a93d756f914ce48ac25c7ba03c13425b
davemgj84 <davemgj@gmail.com>
2020-09-02 21:02:45 +0000

    created registration page and forms - still need to do bug handling - also changed aesthetics of page and buttons

--- express_server.js
+++ express_server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const cookieParser = require('cookie-parser');
-app.use(cookieParser());
-
+// Function to generate random 6 character string:
 const generateRandomString = () => {
   let result = '';
   let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -20,50 +20,91 @@ const generateRandomString = () => {
   return result;
 };
 
+// URL Database Object that stores our key value pairs (short URL and Long URL):
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// Creating a user object to store user data:
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+// initial server setup tests:
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// initial server setup tests:
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// initial server setup tests:
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// get request to handle /urls page - url index and cookies for user login info:
 app.get("/urls", (req, res) => {
   let templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"]
+    user: users[req.cookies["user_id"]]
   };
   res.render("urls_index", templateVars);
 });
 
+// get request to handle new url input at /urls/new and cookies for user login info:
 app.get("/urls/new", (req, res) => {
-  let templateVars = { username: req.cookies["username"] };
+  let templateVars = { user: users[req.cookies["user_id"]] };
   res.render("urls_new", templateVars);
 });
 
+// create register get request:
+app.get("/register", (req, res) => {
+  let templateVars = { user: users[req.cookies["user_id"]] };
+  res.render("register", templateVars);
+});
+
+// create post request to 
+app.post("/register", (req, res) => {
+  const id = generateRandomString();
+   users[id] = {
+    id: id,
+    email: req.body.email,
+    password: req.body.password
+  }
+  res.cookie("user_id", id);
+  res.redirect("/urls");
+});
+
+// handles post request to create new shortURL with generateRandomString function - redirects to /urls:
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
 });
 
+// handles post request to login via /login - creates a username cookie related to login form - redirects to /urls:
 app.post("/login", (req, res) => {
   const username = req.body.username;
   res.cookie("username", username);
   res.redirect("/urls");
 });
 
+// handles post request to logout - clears login info cookie - redirects to /urls:
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+  res.clearCookie("user_id");
   res.redirect("/urls");
 });
 
@@ -73,7 +114,7 @@ app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
     shortURL: shortURL,
     longURL: longURL,
-    username: req.cookies["username"]
+    user: users[req.cookies["user_id"]]
   };
   res.render("urls_show", templateVars);
 });
@@ -95,11 +136,12 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+// allows server to listen for requests:
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-// ********** NOTES FOR EDUCATION PURPOSES **********
+// ********** NOTES FOR EDUCATION PURPOSES FOR EJS SYNTAX**********
 // <%= %>   **** variable insertion
 // <% js %>    ***** js code on the page (ie: loops, if etc) - not visible*
 // <%- include('/path') %>  ***** reusable pieces on each page 'included'
