e7bb1ce39fe084f70f8516dea3a8c044031a57ec
davemgj84 <davemgj@gmail.com>
2020-09-03 23:58:46 +0000

    created encrypted cookies using cookie-sessions - started to modularize the helper functions into separate helper.js file - about to setup mocha chai for testing

--- express_server.js
+++ express_server.js
@@ -2,11 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
+const {findUserByEmail, urlsForUser, generateRandomString} = require('./helpers');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cookieSession({
+  name: "session",
+  keys: ["key"]
+}));
 
 app.set("view engine", "ejs");
 
@@ -25,24 +29,13 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-// Function to generate random 6 character string:
-const generateRandomString = () => {
-  let result = '';
-  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let charactersLength = characters.length;
-  for (let i = 0; i < 6; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-};
-
-// URL Database Object that stores our key value pairs (short URL and Long URL): - EXAMPLE - only takes new users URLs:
+// URL Database Object that stores our key value pairs (short URL and Long URL) - **EXAMPLE** - the users in the static database are currently examples - to test create new user(register):
 const urlDatabase = {
   "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "userRandomID" },
   "9sm5xK": { longURL: "http://www.google.com", userID: "user2RandomID" }
 };
 
-// Creating a user object to store user data: - EXAMPLE - New users really only work now due to bcrypt:
+// Creating a user object to store user database: - EXAMPLE - New users really only work now due to bcrypt:
 const users = {
   "userRandomID": {
     id: "userRandomID",
@@ -56,33 +49,11 @@ const users = {
   }
 };
 
-// Function to help separate urls based on userID.
-const urlsForUser = (id) => {
-  const userUrls = {};
-  for (const url in urlDatabase) {
-    if (urlDatabase[url].userID === id) {
-      userUrls[url] = urlDatabase[url].longURL;
-    }
-  }
-  return userUrls;
-};
-
-// function to find user by email:
-const findUserByEmail = (email) => {
-  for (const userId in users) {
-    const user = users[userId];
-    if (user.email === email) {
-      return user;
-    }
-  }
-  return null;
-};
-
 // get request to show index/collection of urls:
 app.get("/urls", (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session.id;
   let templateVars = {
-    urls: urlsForUser(userID),
+    urls: urlsForUser(userID, urlDatabase),
     user: users[userID]
   };
   res.render("urls_index", templateVars);
@@ -90,13 +61,13 @@ app.get("/urls", (req, res) => {
 
 // get request to show new url input at /urls/new - use cookies for user login info:
 app.get("/urls/new", (req, res) => {
-  let templateVars = { user: users[req.cookies["user_id"]] };
+  let templateVars = { user: users[req.session.id] };
   res.render("urls_new", templateVars);
 });
 
 // get request to show register page:
 app.get("/register", (req, res) => {
-  let templateVars = { user: users[req.cookies["user_id"]] };
+  let templateVars = { user: users[req.session.id] };
   res.render("register", templateVars);
 });
 
@@ -106,7 +77,7 @@ app.post("/register", (req, res) => {
   if (!email || !password) {
     return res.status(400).send("Must fill out Email and Password fields");
   }
-  const foundUser = findUserByEmail(email);
+  const foundUser = findUserByEmail(email, users);
   if (foundUser) {
     return res.status(400).send("Email already registered");
   }
@@ -118,13 +89,13 @@ app.post("/register", (req, res) => {
     password: hash
   };
   users[id] = newUSer;
-  res.cookie("user_id", id);
+  req.session.id = id;
   res.redirect("/urls");
 });
 
 // handles post request to create new shortURL with generateRandomString function - redirects to /urls:
 app.post("/urls", (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session.id;
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = { longURL: req.body.longURL, userID: userID }; // Writing into the database
   res.redirect(`/urls/${shortURL}`);
@@ -132,7 +103,7 @@ app.post("/urls", (req, res) => {
 
 // Get request to show login page:
 app.get("/login", (req, res) => {
-  let templateVars = { user: users[req.cookies["user_id"]] };
+  let templateVars = { user: users[req.session.id] };
   res.render("login", templateVars);
 });
 
@@ -142,13 +113,14 @@ app.post('/login', (req, res) => {
   if (!email || !password) {
     return res.status(403).send("Must fill out Email and Password fields");
   }
-  const foundUser = findUserByEmail(email);
+  const foundUser = findUserByEmail(email, users);
+  console.log(foundUser);
   if (!foundUser) {
     return res.status(403).send("Email not registered");
   }
   const correctPassword = bcrypt.compareSync(password, foundUser.password);
   if (correctPassword) {
-    res.cookie('user_id', foundUser.id);
+    req.session.id = foundUser.id;
     res.redirect('/urls');
   } else {
     return res.send('Incorrect Password');
@@ -157,7 +129,7 @@ app.post('/login', (req, res) => {
 
 // handles post request to logout - clears login info cookie - redirects to /urls:
 app.post("/logout", (req, res) => {
-  res.clearCookie("user_id");
+  req.session = null;
   res.redirect("/urls");
 });
 
@@ -168,7 +140,7 @@ app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
     shortURL: shortURL,
     longURL: longURL,
-    user: users[req.cookies["user_id"]]
+    user: users[req.session.id]
   };
   res.render("urls_show", templateVars);
 });
@@ -189,9 +161,9 @@ app.post("/urls/:id", (req, res) => {
 
 // Post request to handle deleting urls from index - secured for users specific urls and have to be logged in:
 app.post("/urls/:shortURL/delete", (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session.id;
   const shortURL = req.params.shortURL;
-  const urls = urlsForUser(userID);
+  const urls = urlsForUser(userID, urlDatabase);
   const keys = Object.keys(urls);
   if (userID) {
     for (let key of keys) {
@@ -208,5 +180,5 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
 // allows server to listen for requests:
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`TinyApp listening on port ${PORT}!`);
 });
---
+++ helpers.js
@@ -0,0 +1,34 @@
+// function to find user by email within a database:
+const findUserByEmail = (email, database) => {
+  for (const userId in database) {
+    const user = database[userId];
+    if (user.email === email) {
+      return user;
+    }
+  }
+  return null;
+};
+
+// Function to help separate urls based on userID:
+const urlsForUser = (id, database) => {
+  const userUrls = {};
+  for (const url in database) {
+    if (database[url].userID === id) {
+      userUrls[url] = database[url].longURL;
+    }
+  }
+  return userUrls;
+};
+
+// Function to generate random 6 character string:
+const generateRandomString = () => {
+  let result = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
+module.exports = {findUserByEmail, urlsForUser, generateRandomString};
